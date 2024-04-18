3410beeeacb55603e5f38e998d69d34f399d2baa
aengzu <jeon7879@naver.com>
2024-04-18 13:43:16 +0900

    Encode jwt

--- TodoApp/models.py
+++ TodoApp/models.py
@@ -3,18 +3,15 @@ from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
 
 
 class Users(Base):
-    __tablename__= 'users'
-
+    __tablename__ = 'users'
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String, unique=True)
     username = Column(String, unique=True)
     first_name = Column(String)
     last_name = Column(String)
+    role = Column(String)
     hashed_password = Column(String)
     is_active = Column(Boolean, default=True)
-    role = Column(String)
-
-
 
 
 class Todos(Base):
--- TodoApp/routers/auth.py
+++ TodoApp/routers/auth.py
@@ -1,3 +1,5 @@
+from datetime import timedelta, datetime
+
 from fastapi import APIRouter, Depends
 from pydantic import BaseModel
 from sqlalchemy.orm import Session
@@ -7,34 +9,65 @@ from typing import Annotated
 from database import SessionLocal
 from models import Users
 from passlib.context import CryptContext
+from fastapi.security import OAuth2PasswordRequestForm
+
+from jose import jwt
 
 router = APIRouter()
 
+# JWT 하나당 SECRETE_KEY 와 알고리즘 필요함.
+SECRETE_KEY = 'd268a22e7e8597fc23a62f76bd9f59cb8c14bb784c215b4ed08f2269f247bbe0'
+ALGORITHM = 'HS256'
+
 bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
 
-#pydantic
+
+# pydantic
 class CreateUserRequest(BaseModel):
     username: str
     email: str
     first_name: str
-    last_name : str
+    last_name: str
     password: str
     role: str
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
 
 def get_db():
     db = SessionLocal()
     try:
-        yield  db
+        yield db
     finally:
         db.close()
 
+
 db_dependency = Annotated[Session, Depends(get_db)]
-@router.post("/auth", status_code =status.HTTP_201_CREATED)
+
+
+def authenticaate_user(username: str, password: str, db):
+    user = db.query(Users).filter(Users.username == username).first()
+    if not user:
+        return False
+    if not bcrypt_context.verify(password, user.hashed_password):
+        return False
+    return user
+
+
+def create_access_token(username: str, user_id: int, expires_delta: timedelta):
+    encode = {'sub': username, 'id': user_id}
+    expires = datetime.utcnow() + expires_delta
+    encode.update({'exp': expires})
+    return jwt.encode(encode, SECRETE_KEY, algorithm=ALGORITHM)
+
+
+@router.post("/auth", status_code=status.HTTP_201_CREATED)
 async def create_user(db: db_dependency,
-        create_user_request: CreateUserRequest):
+                      create_user_request: CreateUserRequest):
     create_user_model = Users(
         email=create_user_request.email,
-        usedbrname=create_user_request.username,
+        username=create_user_request.username,
         first_name=create_user_request.first_name,
         last_name=create_user_request.last_name,
         role=create_user_request.role,
@@ -46,11 +79,17 @@ async def create_user(db: db_dependency,
     db.commit()
 
 
+@router.post("/token", response_model=Token)
+async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+                                 db: db_dependency):
+    user = authenticaate_user(form_data.username, form_data.password, db)
+    if not user:
+        return 'Failed Authentication'
+    token = create_access_token(user.username, user.id, timedelta(minutes=20))
+    return {'access_token': token, 'token_type': 'bearer'}
 
 
 # 인증. 라우터 패키지 안에 존재
 @router.get("/auth")
 async def get_user():
     return {'user': 'authenticated'}
-
-
